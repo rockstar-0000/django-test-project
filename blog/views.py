f4f9d3cb3d0b5088313360a54f1769b8ec8144a0
rockstar-0000 <tekbrucejin@gmail.com>
2020-04-12 16:29:00 -0500

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,99 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from .models import Post
+
+
+# posts = [
+#     {
+#         'author': 'Jason Green',
+#         'title': 'Blog Post 1',
+#         'content': 'First Post Content',
+#         'date_posted': 'Some time, asshole'
+#     },
+#     {
+#         'author': 'Smack me',
+#         'title': 'Blog Post 2',
+#         'content': 'Second Post Content',
+#         'date_posted': 'Yesterday, asshole'
+#     }
+# ]
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+
+class PostListview(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 10
+
+class UserPostListview(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    # ordering = ['-date_posted']
+    paginate_by = 10
+
+
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
---
+++ users/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+
+def get_upload_path(instance, filename):
+    return '%s/%s' % (instance.user.username, filename)
+
+
+class Profile(models.Model):
+    single_male = "Single Male"
+    single_female = "Single Female"
+    couple = "Couple's Account"
+
+    GENDER_CHOICES = (
+        (single_male, "Single Male"),
+        (single_female, "Single Female"),
+        (couple, 'Couple'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # make bio, current city, gender, etc and other stuff here
+    image = models.ImageField(default='default.jpg', upload_to=get_upload_path)
+    # image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    gender = models.CharField(max_length=25, choices=GENDER_CHOICES, default=single_male)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
