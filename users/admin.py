25ef9369fcc6f0035537521795a05ca26f759e91
rockstar-0000 <tekbrucejin@gmail.com>
2020-05-08 21:55:09 +0430

    Add user admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Profile
+from users.models import Profile, User
+from django.utils.html import format_html
 
-admin.site.register(Profile)
 
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    def image_tag(self, obj):
+        return format_html('<img src="{}" />'.format(obj.image.url))
+
+    image_tag.short_description = 'Image'
+    list_display = ['image_tag', ]
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
