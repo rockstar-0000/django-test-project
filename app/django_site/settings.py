d8d96bc0b7f04be5c4707004163b507df2a48004
rockstar-0000 <tekbrucejin@gmail.com>
2020-05-13 18:04:06 -0500

    changed django host to 127.0.0.1

--- app/django_site/settings.py
+++ app/django_site/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'superuser',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': '5432'
     }
 }
