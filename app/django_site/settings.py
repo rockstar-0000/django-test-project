4dfb7f93fd3a1aba9795c49459e35d4c9cfcf38c
rockstar-0000 <tekbrucejin@gmail.com>
2020-05-13 11:34:33 -0500

    docker working - need to add db files

--- app/django_site/settings.py
+++ app/django_site/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'superuser',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432'
     }
 }
